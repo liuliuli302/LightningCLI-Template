55afebae10b1beead4e6afa0211e66ec8996c9b9
liuliuli302 <liul29956@gmail.com>
2024-04-10 18:07:33 +0800

    finished script change

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,12 @@
+# This is a record file of commonly used script commands
+# and is not a sequence of scripts that should be executed.
+# Therefore, please do not run this file directly,
+# but choose the desired script and manually run it on the command line
+
+# Script that trains the model.
 python main.py fit --config config/config.yaml
+
+# Script that open TensorBoard.
+# Change "version_0" to the correct dir.
+tensorboard --logdir=lightning_logs/version_0
+
